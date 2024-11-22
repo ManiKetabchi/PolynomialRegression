d82fa0521fa0897151e2270dcc284dac809299dc
Mani Ketabchi <maniketabchi@gmail.com>
2024-11-21 19:49:04 -0500

    Polynomial regression working

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 
-X = np.random.rand(100, 1)
-y = 5 + 10 * X ** 2 + 0.2 * np.random.randn(100, 1) # randomly generated data
+X = 5 * np.random.rand(100, 1) - 2
+y = 5 + X**2 + np.random.randn(100, 1) # randomly generated data
+
+poly_features = PolynomialFeatures(degree=2, include_bias=False)
+X_poly = poly_features.fit_transform(X)
 
 reg = LinearRegression()
-reg.fit(X, y)
+reg.fit(X_poly, y)
 
-X_vals = np.linspace(0, 1, 100).reshape(-1, 1)
-y_vals = reg.predict(X_vals)
+X_vals = np.linspace(-2, 2, 100).reshape(-1, 1)
+X_vals_poly = poly_features.transform(X_vals)
+
+
+y_vals = reg.predict(X_vals_poly)
 
 plt.scatter(X, y)
 plt.plot(X_vals, y_vals, color='red')
 plt.show()
-# console log something
-print("Hello World")
